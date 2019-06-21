8d644d2462812c272352ef5731bf3005c08dc0e0
SkyghiS <skyghis@gmail.com>
2019-06-21 16:07:21 +0200

    Auto code format, clean imports and uniformize line ending.

--- j-interop/src/org/jinterop/dcom/test/Test.java
+++ j-interop/src/org/jinterop/dcom/test/Test.java
@@ -4,35 +4,33 @@ import org.jinterop.dcom.core.JIComServer;
 
 public class Test {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 //	 try {
-     boolean b = null instanceof JIComServer ? false : true;
-		// Socket socket = new Socket("***REMOVED***",139);
-		 //socket.close();
+        boolean b = null instanceof JIComServer ? false : true;
+        // Socket socket = new Socket("***REMOVED***",139);
+        //socket.close();
 //	 UniAddress mydomaincontoller = UniAddress.getByName( "192.168.170.6" );
 //	 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "***REMOVED***", "***REMOVED***", "***REMOVED***" );
 //
 //	 SmbSession.logon( mydomaincontoller, mycreds );
-	//	 Config.setProperty("jcifs.smb.client.laddr","***REMOVED***");
-	//	 Config.setProperty("jcifs.smb.client.domain","***REMOVED***");
+        //	 Config.setProperty("jcifs.smb.client.laddr","***REMOVED***");
+        //	 Config.setProperty("jcifs.smb.client.domain","***REMOVED***");
 //		 System.setProperty("jcifs.smb.client.laddr","***REMOVED***");
 //		 System.setProperty("jcifs.smb.client.domain","***REMOVED***");
 //		 System.setProperty("jcifs.netbios.hostname","***REMOVED***");
 
-		 //NtlmChallenge challenge = SmbSession.getChallengeForDomain();
-
+        //NtlmChallenge challenge = SmbSession.getChallengeForDomain();
 //		 UniAddress mydomaincontoller = UniAddress.getByName("***REMOVED***");
 //		 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "***REMOVED***", "***REMOVED***", "***REMOVED***" );
 //		 SmbSession.logon(mydomaincontoller,mycreds);
-		 //PLEASE NOTE THAT THE WINDOWS "SERVER" SERVICE SOULD BE RUNNING !!! OTHERWISE THE
-		 //GETCHALLENGE WILL FAIL.
+        //PLEASE NOTE THAT THE WINDOWS "SERVER" SERVICE SOULD BE RUNNING !!! OTHERWISE THE
+        //GETCHALLENGE WILL FAIL.
 //		 UniAddress mydomaincontoller = UniAddress.getByName("***REMOVED***");
 //		 byte[] b =  SmbSession.getChallenge(mydomaincontoller,139);
-		 int i = 0;
-		// NtlmChallenge challenge = SmbSession.getChallengeForDomain();
+        int i = 0;
+        // NtlmChallenge challenge = SmbSession.getChallengeForDomain();
 
-	     // SUCCESS
-
+        // SUCCESS
 //	 } catch( SmbAuthException sae ) {
 //	     // AUTHENTICATION FAILURE
 //	     sae.printStackTrace();
@@ -46,11 +44,10 @@ public class Test {
 //		// TODO Auto-generated catch block
 //		e.printStackTrace();
 //	}
-}
+    }
 
- public void test()
- 	{
- 		System.out.println("Called back !!!");
- 	}
+    public void test() {
+        System.out.println("Called back !!!");
+    }
 
 }
