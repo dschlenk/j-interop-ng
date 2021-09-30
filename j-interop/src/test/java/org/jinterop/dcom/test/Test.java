e6aefe269ceaf3adcb862da3142f742f47dd271c
David Schlenk <dschlenk@convergeone.com>
2021-09-30 15:00:00 -0500

    remove someone's creds

--- j-interop/src/test/java/org/jinterop/dcom/test/Test.java
+++ j-interop/src/test/java/org/jinterop/dcom/test/Test.java
@@ -7,25 +7,25 @@ public class Test {
     public static void main(String[] args) {
 //	 try {
         boolean b = !(null instanceof JIComServer);
-        // Socket socket = new Socket("***REMOVED***",139);
+        // Socket socket = new Socket("10.24.10.65",139);
         //socket.close();
-//	 UniAddress mydomaincontoller = UniAddress.getByName( "192.168.170.6" );
-//	 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "***REMOVED***", "***REMOVED***", "***REMOVED***" );
+//	 UniAddress mydomaincontoller = UniAddress.getByName( "" );
+//	 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "", "", "" );
 //
 //	 SmbSession.logon( mydomaincontoller, mycreds );
-        //	 Config.setProperty("jcifs.smb.client.laddr","***REMOVED***");
-        //	 Config.setProperty("jcifs.smb.client.domain","***REMOVED***");
-//		 System.setProperty("jcifs.smb.client.laddr","***REMOVED***");
-//		 System.setProperty("jcifs.smb.client.domain","***REMOVED***");
-//		 System.setProperty("jcifs.netbios.hostname","***REMOVED***");
+        //	 Config.setProperty("jcifs.smb.client.laddr","");
+        //	 Config.setProperty("jcifs.smb.client.domain","");
+//		 System.setProperty("jcifs.smb.client.laddr","");
+//		 System.setProperty("jcifs.smb.client.domain","");
+//		 System.setProperty("jcifs.netbios.hostname","");
 
         //NtlmChallenge challenge = SmbSession.getChallengeForDomain();
-//		 UniAddress mydomaincontoller = UniAddress.getByName("***REMOVED***");
-//		 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "***REMOVED***", "***REMOVED***", "***REMOVED***" );
+//		 UniAddress mydomaincontoller = UniAddress.getByName("");
+//		 NtlmPasswordAuthentication mycreds = new NtlmPasswordAuthentication( "", "", "" );
 //		 SmbSession.logon(mydomaincontoller,mycreds);
         //PLEASE NOTE THAT THE WINDOWS "SERVER" SERVICE SOULD BE RUNNING !!! OTHERWISE THE
         //GETCHALLENGE WILL FAIL.
-//		 UniAddress mydomaincontoller = UniAddress.getByName("***REMOVED***");
+//		 UniAddress mydomaincontoller = UniAddress.getByName("");
 //		 byte[] b =  SmbSession.getChallenge(mydomaincontoller,139);
         int i = 0;
         // NtlmChallenge challenge = SmbSession.getChallengeForDomain();
